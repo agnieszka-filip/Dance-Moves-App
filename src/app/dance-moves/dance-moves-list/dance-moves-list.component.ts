f6e74928d873e4d8ff32e6c8b73a759f35ffa265
Agnieszka F <agnieszka.fi94@gmail.com>
2020-03-06 12:45:52 +0100

    Set up a service

--- src/app/dance-moves/dance-moves-list/dance-moves-list.component.ts
+++ src/app/dance-moves/dance-moves-list/dance-moves-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { DanceMove } from './dance-move.model';
+import { DanceMovesService } from '../dance-moves.service';
 
 @Component({
   selector: 'app-dance-moves-list',
@@ -8,15 +9,13 @@ import { DanceMove } from './dance-move.model';
 })
 export class DanceMovesListComponent implements OnInit {
   @Output() danceMoveWasClicked = new EventEmitter<DanceMove>();
-  danceMoves: DanceMove[] = [
-    new DanceMove('Elbow Aysha', 'This is just a test',
-    'https://www.stockvault.net/data/2017/02/12/222400/preview16.jpg'),
-    new DanceMove('Twisted Grip Hangman', 'This is just a test',
-    'https://www.stockvault.net/data/2017/02/12/222401/preview16.jpg')
-  ];
+  danceMoves: DanceMove[];
   
+  constructor(private danceMovesService: DanceMovesService) {
+  } 
 
   ngOnInit() {
+    this.danceMoves = this.danceMovesService.getDanceMoves();
   }
 
   onDanceMoveClicked(danceMove: DanceMove): void {
--- src/app/dance-moves/dance-moves.component.ts
+++ src/app/dance-moves/dance-moves.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { DanceMove } from './dance-moves-list/dance-move.model';
+import { DanceMovesService } from './dance-moves.service';
 
 @Component({
   selector: 'app-dance-moves',
   templateUrl: './dance-moves.component.html',
-  styleUrls: ['./dance-moves.component.scss']
+  styleUrls: ['./dance-moves.component.scss'],
+  providers: [DanceMovesService]
 })
 export class DanceMovesComponent implements OnInit {
   clickedDanceMove: DanceMove;
