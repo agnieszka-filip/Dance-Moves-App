fd1135b144d6e969b056726d50fd8fe41779c164
Agnieszka <agnieszka.fi94@gmail.com>
2020-04-12 18:54:59 +0200

    Add required skills to dance-move-details display

--- src/app/dance-moves/dance-moves-list/dance-move.model.ts
+++ src/app/dance-moves/dance-moves-list/dance-move.model.ts
@@ -1,10 +1,12 @@
+import { RequiredSkill } from '../required-skill.model';
+
 export class DanceMove {
     public name: string;
     public description: string;
     public imageVideoPath: string;
-    public requiredSkills: any;
+    public requiredSkills: RequiredSkill[];
 
-    constructor (name: string, desc: string, imgVidPath: string, requiredSkills: any) {
+    constructor (name: string, desc: string, imgVidPath: string, requiredSkills: RequiredSkill[]) {
         this.name = name;
         this.description = desc;
         this.imageVideoPath = imgVidPath;
