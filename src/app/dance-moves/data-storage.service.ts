a6008fd6d37f6e386cf7e82217ff9dc2b422cfd0
agnieszka-filip <agnieszka.fi94@gmail.com>
2020-04-25 23:32:15 +0200

    Allow to store data in Firebase

--- src/app/dance-moves/data-storage.service.ts
+++ src/app/dance-moves/data-storage.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DanceMovesService } from './dance-moves.service';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient,
+        private danceMovesService: DanceMovesService) {}
+    
+    storeDanceMoves() {
+        const danceMoves = this.danceMovesService.getDanceMoves();
+        this.http
+            .put('https://dance-moves-5afda.firebaseio.com/dance-moves.json', danceMoves)
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
+import { DataStorageService } from '../dance-moves/data-storage.service';
+
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+    constructor(private dataStorageService: DataStorageService) { }
+    
+    onSaveData() {
+        this.dataStorageService.storeDanceMoves();
+    }
 }
