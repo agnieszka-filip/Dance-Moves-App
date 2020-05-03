aa6c27e93ff5155aefda4bc3d77bbf955c26bc5c
agnieszka-filip <agnieszka.fi94@gmail.com>
2020-05-03 16:01:43 +0200

    Create the Shorten pipe, add functionality to the authentication form inputs and improve styling in the dance-moves-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
-import { ReactiveFormsModule, FormsModule } from '@angular/forms'
-import { HttpClientModule } from '@angular/common/http'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component'
-import { HeaderComponent } from './header/header.component'
-import { DanceMovesComponent } from './dance-moves/dance-moves.component'
-import { DanceMovesListComponent } from './dance-moves/dance-moves-list/dance-moves-list.component'
-import { DanceMovesDetailsComponent } from './dance-moves/dance-moves-details/dance-moves-details.component'
-import { DanceMoveItemComponent } from './dance-moves/dance-moves-list/dance-move-item/dance-move-item.component'
-import { DropdownDirective } from './dropdown.directive'
-import { AppRoutingModule } from './app-routing.module'
-import { DanceMovesStartComponent } from './dance-moves/dance-moves-start/dance-moves-start.component'
-import { DanceMoveEditComponent } from './dance-moves/dance-move-edit/dance-move-edit.component'
-import { DanceMovesService } from './dance-moves/dance-moves.service'
-import { DataStorageService } from './dance-moves/data-storage.service'
-import { DanceMovesResolverService } from './dance-moves/dance-moves-resolver.service';
-import { AuthComponent } from './auth/auth.component'
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { DanceMovesComponent } from "./dance-moves/dance-moves.component";
+import { DanceMovesListComponent } from "./dance-moves/dance-moves-list/dance-moves-list.component";
+import { DanceMovesDetailsComponent } from "./dance-moves/dance-moves-details/dance-moves-details.component";
+import { DanceMoveItemComponent } from "./dance-moves/dance-moves-list/dance-move-item/dance-move-item.component";
+import { DropdownDirective } from "./dropdown.directive";
+import { AppRoutingModule } from "./app-routing.module";
+import { DanceMovesStartComponent } from "./dance-moves/dance-moves-start/dance-moves-start.component";
+import { DanceMoveEditComponent } from "./dance-moves/dance-move-edit/dance-move-edit.component";
+import { DanceMovesService } from "./dance-moves/dance-moves.service";
+import { DataStorageService } from "./dance-moves/data-storage.service";
+import { DanceMovesResolverService } from "./dance-moves/dance-moves-resolver.service";
+import { AuthComponent } from "./auth/auth.component";
+import { ShortenPipe } from "./shorten.pipe";
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { AuthComponent } from './auth/auth.component'
     DanceMovesStartComponent,
     DanceMoveEditComponent,
     AuthComponent,
+    ShortenPipe,
   ],
   imports: [
     BrowserModule,
