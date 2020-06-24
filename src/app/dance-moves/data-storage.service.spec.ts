18b7b7822bf0ad14f9b5ea906a3c393b5e53e4d8
agnieszka-filip <agnieszka.fi94@gmail.com>
2020-06-24 16:10:05 +0200

    Add a test for the data storage service

--- src/app/dance-moves/data-storage.service.spec.ts
+++ src/app/dance-moves/data-storage.service.spec.ts
@@ -1,49 +1,42 @@
-import { TestBed, getTestBed } from "@angular/core/testing";
-import {
-  HttpClientTestingModule,
-  HttpTestingController,
-} from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+
 import { DataStorageService } from "./data-storage.service";
+import { HttpClientModule } from "@angular/common/http";
 import { DanceMovesService } from "./dance-moves.service";
+import { DanceMove } from "./dance-moves-list/dance-move.model";
 
 describe("DataStorageService", () => {
-  let injector: TestBed;
   let service: DataStorageService;
-  let httpTestingCont: HttpTestingController;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientModule],
       providers: [DataStorageService, DanceMovesService],
     });
-    injector = getTestBed();
-    service = injector.get(DataStorageService);
-    httpTestingCont = injector.get(HttpTestingController);
+    service = TestBed.get(DataStorageService);
   });
 
-  /*afterEach(() => {
-    httpTestingCont.verify();
+  it("should be created", () => {
+    const service: DataStorageService = TestBed.get(DataStorageService);
+    expect(service).toBeTruthy();
   });
+});
 
-  it("should return an Observable<DanceMove[]>", () => {
-    const dummyMove = [
-      {
-        requiredSkills: "Invert",
-        name: "Gemini",
-        description: "A dance move",
-        imageVideoPath: "http://ddd",
-      },
-    ];
-
-    service.fetchDanceMoves().subscribe((danceMoves) => {
-      expect(danceMoves.length).toBe(1);
-      expect(danceMoves).toEqual(this.dummyMove);
-    });
-
-    const req = httpTestingCont.expectOne(
-      `${service["https://dance-moves-5afda.firebaseio.com/dance-moves.json"]}/dance-moves`
-    );
-    expect(req.request.method).toBe("GET");
-    req.flush(dummyMove);
-  });*/
+it("should retrieve dance moves from Firebase via GET", () => {
+  TestBed.configureTestingModule({
+    imports: [HttpClientModule],
+    providers: [DataStorageService, DanceMovesService],
+  });
+  const service: DataStorageService = TestBed.get(DataStorageService);
+  const dummyMoves: DanceMove[] = [
+    {
+      name: "Allegra",
+      description: "Test",
+      imageVideoPath: "http://test.pl",
+      requiredSkills: [],
+    },
+  ];
+  service.fetchDanceMoves().subscribe((dummyMoves) => {
+    expect(dummyMoves.length).toBe(1);
+    expect(dummyMoves).toEqual(dummyMoves);
+  });
 });
