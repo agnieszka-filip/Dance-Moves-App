c4ce326ed11f84bfe98f25064d25bd5c4fa9353f
agnieszka-filip <agnieszka.fi94@gmail.com>
2020-07-02 01:35:24 +0200

    Fix the test for the dance moves details component

--- src/app/dance-moves/dance-moves-details/dance-moves-details.component.spec.ts
+++ src/app/dance-moves/dance-moves-details/dance-moves-details.component.spec.ts
@@ -1,21 +1,18 @@
-import {
-  TestBed,
-  ComponentFixture,
-  fakeAsync,
-  tick,
-} from "@angular/core/testing";
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { ActivatedRoute, Router, Params } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { of, from } from "rxjs";
+
 import { DanceMovesDetailsComponent } from "./dance-moves-details.component";
 import { DanceMovesService } from "../dance-moves.service";
-import { ActivatedRoute, Router, Params } from "@angular/router";
-import { of, from } from "rxjs";
 import { DropdownDirective } from "./.././../dropdown.directive";
-import { RouterTestingModule } from "@angular/router/testing";
-import { By } from "@angular/platform-browser";
+import { DanceMove } from "../dance-moves-list/dance-move.model";
 
 describe("Dance Moves Details Component", () => {
   let fixture: ComponentFixture<DanceMovesDetailsComponent>;
   let router: Router;
-  let mockActivatedRoute, mockDanceMovesService, mockRouter;
+  let mockDanceMovesService;
+  let danceMove: DanceMove;
 
   beforeEach(() => {
     mockDanceMovesService = jasmine.createSpyObj(["getDanceMove"]);
@@ -24,32 +21,28 @@ describe("Dance Moves Details Component", () => {
       declarations: [DropdownDirective, DanceMovesDetailsComponent],
       imports: [RouterTestingModule.withRoutes([])],
       providers: [
-        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: ActivatedRoute, useValue: { params: from([{ id: "1" }]) } },
         { provide: DanceMovesService, useValue: mockDanceMovesService },
-        {
-          provide: ActivatedRoute,
-          useValue: { params: from([{ id: 1 }]) },
-        },
       ],
     });
     fixture = TestBed.createComponent(DanceMovesDetailsComponent);
     router = TestBed.get(Router);
+    danceMove = {
+      name: "Seahorse",
+      description: "A very pretty move",
+      imageVideoPath: "",
+      requiredSkills: [],
+    };
 
-    mockDanceMovesService.getDanceMove.and.returnValue(
-      of({
-        id: 1,
-        name: "Seahorse",
-        description: "A very pretty move",
-        requiredSkills: [],
-      })
-    );
+    mockDanceMovesService.getDanceMove.and.returnValue(danceMove);
   });
 
-  it("should render the name of the dance move in the h2 tag", () => {
+  it("should render the name of the dance move in the h2 tag", (done) => {
     fixture.detectChanges();
 
     expect(fixture.nativeElement.querySelector("h1").textContent).toContain(
       "Seahorse"
     );
+    done();
   });
 });
